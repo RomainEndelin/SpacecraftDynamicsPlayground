14d1141ae2da375ec40de31ed5e80d4df6db1dfa
Romain Endelin <rendelin@zendesk.com>
2020-08-30 15:33:53 +1000

    first latex cleanup

--- space_sim/latex/latex_to_pdf.py
+++ space_sim/latex/latex_to_pdf.py
@@ -1,16 +1,15 @@
 import os
 import shutil
-import tempfile
 from subprocess import Popen
 
 
 def compile_tex(rendered_tex, out_pdf_path):
-    tmp_dir = tempfile.mkdtemp()
-    in_tmp_path = os.path.join(tmp_dir, "rendered.tex")
+    latex_build_dir = os.path.dirname(os.path.join("builds", "latex"))
+    in_tmp_path = os.path.join(latex_build_dir, "assets", "rendered.tex")
     with open(in_tmp_path, "w") as outfile:
         outfile.write(rendered_tex)
-    out_tmp_path = os.path.join(tmp_dir, "rendered.pdf")
-    p = Popen(["pdflatex", "-output-directory", tmp_dir, in_tmp_path])
+    out_build_path = os.path.join(latex_build_dir, "build")
+    out_build_file = os.path.join(out_build_path, "rendered.pdf")
+    p = Popen(["pdflatex", "-output-directory", out_build_file, in_tmp_path])
     p.communicate()
-    shutil.copy(out_tmp_path, out_pdf_path)
-    shutil.rmtree(tmp_dir)
+    shutil.copy(out_build_file, out_pdf_path)
